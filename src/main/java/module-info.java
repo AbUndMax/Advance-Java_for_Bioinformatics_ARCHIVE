ec001494c38968d5fa6d62154d87f4c63d4aa625
Niklas <Max_Niklas@outlook.de>
2025-05-12 09:50:44 +0200

    update module-info.java with assignment03

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,7 @@ module AdJa.git.classroom {
     requires javafx.fxml;
     requires javafx.graphics;
     exports assignment02;
+
+    opens assignment03.window to javafx.fxml;
+    exports assignment03;
 }
