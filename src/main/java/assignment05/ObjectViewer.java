dec261db3716aba0328dbda0b86d4c539501ae74
Niklas <Max_Niklas@outlook.de>
2025-05-22 14:38:05 +0200

    Add explanation for .obj file data (Task 3) in README_asgmt05.md and add DocString to ObjectViewer.java
    Detailed explanations of .obj file elements (v, vt, vn, f) were added to the README, clarifying 3D object definitions. Additionally, Javadoc comments were introduced in `ObjectViewer.java` to describe its purpose and application lifecycle responsibilities.

--- src/main/java/assignment05/ObjectViewer.java
+++ src/main/java/assignment05/ObjectViewer.java
@@ -5,6 +5,16 @@ import assignment05.window.WindowView;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * The ObjectViewer class serves as the main entry point for launching the
+ * Object Viewer application. It initializes the primary stage with a specified
+ * scene containing a 3D object viewer interface.
+ *
+ * This class extends the JavaFX `Application` class, making it responsible for
+ * setting up and running the JavaFX application lifecycle.
+ *
+ * author: Luis Reimer, Niklas Gerbes
+ */
 public class ObjectViewer extends Application {
 
     public void start(Stage primaryStage) throws Exception {
