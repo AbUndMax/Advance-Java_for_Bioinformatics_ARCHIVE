2ff3073adf7db7e15230ae8b670be0e784e48a13
Niklas <Max_Niklas@outlook.de>
2025-06-16 20:29:24 +0200

    finish basic startup GUI (implement #1)

--- project/src/main/java/explorer/window/controller/SelectionController.java
+++ project/src/main/java/explorer/window/controller/SelectionController.java
@@ -1,8 +1,13 @@
 package explorer.window.controller;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.SplitPane;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.BorderPane;
 
 public class SelectionController {
     @FXML
@@ -21,11 +26,49 @@ public class SelectionController {
     private SplitPane selectionSplitPane;
 
     @FXML
+    private SplitPane selectionListSplitPane;
+
+    @FXML
+    private ToggleButton selectionListToggle;
+
+    @FXML
+    private BorderPane selectionListPane;
+
+    @FXML
     public void initialize() {
+        setDividerControls();
+        setSelectionListControls();
+    }
+
+    /**
+     * sets the corresponding actions for the divider buttons between the two treeViews
+     */
+    private void setDividerControls() {
         centerDivider1.setOnAction(e -> selectionSplitPane.setDividerPositions(0.5));
         centerDivider2.setOnAction(e -> selectionSplitPane.setDividerPositions(0.5));
 
         expandIsAView.setOnAction(e -> selectionSplitPane.setDividerPositions(0));
         expandPartOfView.setOnAction(e -> selectionSplitPane.setDividerPositions(1));
+    }
+
+    /**
+     * sets the correct toggle action for the selectionList panel
+     */
+    private void setSelectionListControls() {
+        selectionListSplitPane.getItems().remove(selectionListPane);
+
+        // listening to the toggle button
+        selectionListToggle.selectedProperty().addListener((obs, wasSelected, isSelected) -> {
+            ObservableList<Node> items = selectionListSplitPane.getItems();
+
+            if (isSelected && !items.contains(selectionListPane)) {
+                // if button is selected, add the selectionPane and set the divider to the most right side
+                items.add(selectionListPane);
+                selectionListSplitPane.setDividerPositions(1);
+            } else if (!isSelected && items.contains(selectionListPane)) {
+                // if not selected, remove the selectionListPane
+                items.remove(selectionListPane);
+            }
+        });
     }
 }
--- project/src/main/java/explorer/window/controller/VisualizationController.java
+++ project/src/main/java/explorer/window/controller/VisualizationController.java
@@ -1,4 +1,14 @@
 package explorer.window.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
 public class VisualizationController {
+    @FXML
+    private ChoiceBox<String> searchChoice;
+
+    @FXML
+    public void initialize() {
+        searchChoice.setValue("3D viewer");
+    }
 }
